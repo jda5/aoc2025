93bd0100c1d1f675915a37eee3b1545f92f1a243
Jacob Strauss <jacob.d.strauss@gmail.com>
2025-12-04 09:46:06 +0000

    Update main to use puzzle 4 implementation and add RemoveRolls function

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	puzzles "github.com/jda5/aoc2025/puzzles/03"
+	puzzles "github.com/jda5/aoc2025/puzzles/04"
 	"github.com/jda5/aoc2025/utils"
 )
 
 func main() {
 
-	input := utils.ReadInputLines("03")
+	input := utils.ReadInputLines("04")
 
-	fmt.Println(puzzles.TotalJoltage(input))
+	fmt.Println(puzzles.RemoveRolls(input))
 }
---
+++ puzzles/04/printing_department.go
@@ -0,0 +1,135 @@
+package puzzles
+
+import "strings"
+
+// loc = rowIdx, columnIdx
+
+// Set is a collection of unique elements
+type Set map[[2]int]struct{}
+
+func getAdjacentRolls(row int, col int, grid []string) [][2]int {
+
+	positions := make([][2]int, 0)
+
+	delta := [3]int{1, 0, -1}
+
+	for _, i := range delta {
+		for _, j := range delta {
+
+			if i == 0 && j == 0 {
+				continue
+			}
+
+			dy := row + i
+			dx := col + j
+
+			if dy < 0 || dy >= len(grid) {
+				continue
+			}
+
+			if dx < 0 || dx >= len(grid[0]) {
+				continue
+			}
+
+			val := string(grid[dy][dx])
+
+			if val != "@" {
+				continue
+			}
+
+			positions = append(positions, [2]int{dy, dx})
+		}
+	}
+
+	return positions
+}
+
+func CountRolls(grid []string) int {
+
+	locations := make(Set, 0)
+
+	for rowIdx, row := range grid {
+		for colIdx, square := range row {
+
+			if string(square) != "@" {
+				continue
+			}
+
+			coors := getAdjacentRolls(rowIdx, colIdx, grid)
+
+			if len(coors) >= 4 {
+				continue
+			}
+
+			loc := [2]int{rowIdx, colIdx}
+
+			locations[loc] = struct{}{}
+
+		}
+	}
+
+	return len(locations)
+}
+
+func RemoveRolls(input []string) int {
+
+	removed := 0
+	grid := input
+
+	for {
+
+		locations := make(Set, 0)
+		removedRolls := false
+
+		for rowIdx, row := range grid {
+			for colIdx, square := range row {
+
+				if string(square) != "@" {
+					continue
+				}
+
+				coors := getAdjacentRolls(rowIdx, colIdx, grid)
+
+				if len(coors) >= 4 {
+					continue
+				}
+
+				loc := [2]int{rowIdx, colIdx}
+
+				locations[loc] = struct{}{}
+			}
+		}
+
+		// create a new grid with the rolls remove
+		newGrid := make([]string, 0)
+		for rowIdx, row := range grid {
+
+			var sb strings.Builder
+
+			for colIdx, square := range row {
+
+				loc := [2]int{rowIdx, colIdx}
+
+				_, ok := locations[loc]
+				if ok {
+					sb.WriteString(".")
+					removed++
+					removedRolls = true
+				} else {
+					sb.WriteString(string(square))
+				}
+
+			}
+			newGrid = append(newGrid, sb.String())
+		}
+
+		if !removedRolls {
+			break
+		}
+
+		grid = newGrid
+
+	}
+
+	return removed
+}
